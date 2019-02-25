648ffb08613ec63b8201998800fdba3bdfdd1d97
juhikaushik02 <juhikaushik02@gmail.com>
2019-02-25 16:12:32 +0530

    splash screen

--- app/src/main/java/com/dxdevil/pd/prjp/MainActivity.kt
+++ app/src/main/java/com/dxdevil/pd/prjp/MainActivity.kt
@@ -1,23 +1,32 @@
+
 package com.dxdevil.pd.prjp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import androidx.core.content.ContextCompat.startActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Handler().postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }, 5000)
+    }
+}
 
-        val background = object : Thread() {
+       /*  val background = object : Thread() {
             override fun run() {
-
                 try {
                     // Thread will sleep for 5 seconds
-                    Thread.sleep(2000)
+                    Thread.sleep((5 * 1000).toLong())
 
                     // After 5 seconds redirect to another intent
-                    val i = Intent(applicationContext, LoginActivity::class.java)
+                    val i = Intent(baseContext, MainActivity::class.java)
                     startActivity(i)
                     //Remove activity
                     finish()
@@ -30,15 +39,8 @@ class MainActivity : AppCompatActivity() {
         // start thread
         background.start()
     }
-}
+    }*/
 
 
-            var backg = object : Thread(){
-                override fun run() {
-                    super.run()
-                    Thread.sleep(1000)
-                startActivity(Intent(applicationContext,LoginActivity::class.java))
-                }
-            }
-            }
-    }
+
+
