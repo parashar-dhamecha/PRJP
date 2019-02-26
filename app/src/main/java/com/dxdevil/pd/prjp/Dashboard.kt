9333df3cd22a24c049fd7d1c5cf46b6aeebd1412
rishabh90993 <rishabh90993@gmail.com>
2019-02-26 19:21:15 +0530

    Rishabh popup added

--- app/src/main/java/com/dxdevil/pd/prjp/Dashboard.kt
+++ app/src/main/java/com/dxdevil/pd/prjp/Dashboard.kt
@@ -1,5 +1,9 @@
 package com.dxdevil.pd.prjp
 
+import android.annotation.SuppressLint
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
@@ -11,11 +15,15 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_dashboard.*
+import kotlinx.android.synthetic.main.signpopup.*
 
 
 class Dashboard : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var ntoggle : ActionBarDrawerToggle
+
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
@@ -30,6 +38,16 @@ class Dashboard : AppCompatActivity() {
             Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
                 .setAction("Action", null)
                 .show()
+                    addsign.setOnClickListener {
+                        var dialog :Dialog=Dialog(this)
+                        dialog.setContentView(R.layout.signpopup)
+                        dialog.setTitle("@string/addsign")
+                        popuptv.setOnClickListener {
+                            dialog.dismiss()
+                        }
+                        dialog.window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                        dialog.show()
+                    }
 
             }
         }
@@ -39,6 +57,9 @@ class Dashboard : AppCompatActivity() {
             return true
         return super.onOptionsItemSelected(item)
     }
+
+
+
     }
 
 
