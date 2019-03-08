fbdc4158b60ba6a363f75f14f91085e3fd04cba6
juhikaushik02 <juhikaushik02@gmail.com>
2019-03-08 16:24:36 +0530

    Merge branch 'master' of G:\project\PRJP with conflicts.

--- app/src/main/java/com/dxdevil/pd/prjp/Contacts.kt
+++ app/src/main/java/com/dxdevil/pd/prjp/Contacts.kt
@@ -1,47 +1,51 @@
 package com.dxdevil.pd.prjp
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Adapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_contacts.*
-import kotlinx.android.synthetic.main.contactsadapter.view.*
 
 class Contacts : AppCompatActivity() {
 
-   private var adapter:ContactsAdapter?=null
-    private var contactList: ArrayList<ModelClass>?=null
-    private var layoutManager:RecyclerView.LayoutManager?=null
+    private var adapter:ContactsAdapter?=null
+    private var recyclerView: RecyclerView? = null
+
+    private var Con: ArrayList<String>?=null
+    private var layoutManager: RecyclerView.LayoutManager? = null
+
+
+    private val myImageList = intArrayOf(R.drawable.user, R.drawable.user, R.drawable.user, R.drawable.user, R.drawable.user, R.drawable.user, R.drawable.user)
+    private val myImageNameList = arrayOf("Dhruv", "Yogita", "Parashar", "Rishabh", "p", "Harly", "Lamborghini", "Silver")
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_contacts)
+
+
+        layoutManager = LinearLayoutManager(this)
 
 
 
+        recyclerView = findViewById(R.id.r1) as RecyclerView
 
-        contactList=ArrayList<ModelClass>()
-        layoutManager=LinearLayoutManager(this)
-        adapter=ContactsAdapter(contactList!!,this)
 
-        recyclerView.layoutManager=layoutManager
-        recyclerView.adapter=adapter
+        adapter = ContactsAdapter(this, Con!!)
+        recyclerView!!.adapter = adapter
 
-        for ( i in 0..9 ){
-            val modelClass=ModelClass
-            modelClass.name="juhi"+i
-            modelClass.name="seema"+i
-
-            contactList!!.add((modelClass))
-        }
         adapter!!.notifyDataSetChanged()
 
 
 
 
     }
+
 
 
 }
--- app/src/main/java/com/dxdevil/pd/prjp/ContactsAdapter.kt
+++ app/src/main/java/com/dxdevil/pd/prjp/ContactsAdapter.kt
@@ -4,34 +4,50 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.*
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+ class ContactsAdapter( private val context: Context, private val Con: ArrayList<Int> ): RecyclerView.Adapter<ContactsAdapter.ViewHolder>() {
 
 
+     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.contactsadapter, parent, false)
 public class ContactsAdapter<Context>(private val list: ArrayList<ModelClass>,
                                private val context:Context) : RecyclerView.Adapter<ContactsAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.contactsadapter, parent, false)
 
-        return RecyclerView.ViewHolder(view)
+
+        return ViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        return list.size
+        return Con.size
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        holder?.bindItem(list[position])
-    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.pro.setImageResource(Con[position])
+        holder.name.setText(Con[position])
+//       holder.del.setButton(Con[position].getImage_drawables())
+
+
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bindItem(modelClass: ModelClass) {
-            var name: TextView = itemView.findViewById(R.id.name) as TextView
-            name.text = modelClass.name
+
+            var name: TextView
+            var pro: ImageView
+  //          var del: Button
+
+            init {
+
+                name = itemView.findViewById(R.id.name) as TextView
+                pro = itemView.findViewById(R.id.pro) as ImageView
+    //            del = itemView.findViewById(R.id.del) as Button
+
+            }
         }
     }
-}
+
 
 
 
