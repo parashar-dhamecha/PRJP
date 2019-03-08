2f0490215e8a1ebcd6c8603702f017228ad4ec61
Pratik-Goswami <goswamipratik666@gmail.com>
2019-03-08 11:59:54 +0530

    Profile page Added

--- app/src/main/java/com/dxdevil/pd/prjp/ContactsAdapter.kt
+++ app/src/main/java/com/dxdevil/pd/prjp/ContactsAdapter.kt
@@ -1,34 +1,35 @@
 package com.dxdevil.pd.prjp
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
 
 public class ContactsAdapter<Context>(private val list: ArrayList<ModelClass>,
-                               private val context :Context) : RecyclerView.Adapter<ContactsAdapter.ViewHolder>()
-{
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view= LayoutInflater.from(context).inflate(R.layout.contactsadapter,parent,false)
+                               private val context:Context) : RecyclerView.Adapter<ContactsAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.contactsadapter, parent, false)
 
-        return ViewHolder(view)
+        return RecyclerView.ViewHolder(view)
     }
 
     override fun getItemCount(): Int {
         return list.size
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         holder?.bindItem(list[position])
     }
 
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        fun bindItem(modelClass: ModelClass)
-            {
-                var name:TextView=itemView.findViewById(R.id.name) as TextView
-                name.text=modelClass.name
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bindItem(modelClass: ModelClass) {
+            var name: TextView = itemView.findViewById(R.id.name) as TextView
+            name.text = modelClass.name
+        }
     }
 }
 
