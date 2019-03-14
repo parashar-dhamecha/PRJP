0bb6029c8721c71ab92a9748f94b62fb366810de
Pratik-Goswami <goswamipratik666@gmail.com>
2019-03-14 16:28:33 +0530

    Change is the only thing that is permanent

--- app/src/main/java/com/dxdevil/pd/prjp/ResetPasswordActivity.kt
+++ app/src/main/java/com/dxdevil/pd/prjp/ResetPasswordActivity.kt
@@ -1,12 +1,16 @@
 package com.dxdevil.pd.prjp
 
-import android.annotation.SuppressLint
+
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_forgotpass.*
 
 class ResetPasswordActivity(): AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reset_password)
+
+
     }
 }
