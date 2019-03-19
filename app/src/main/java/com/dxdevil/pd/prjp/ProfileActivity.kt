e22a61bdf51f25a95a51169b7b6fd92a7394debb
Pratik-Goswami <goswamipratik666@gmail.com>
2019-03-19 21:29:02 +0530

    pro

--- app/src/main/java/com/dxdevil/pd/prjp/ProfileActivity.kt
+++ app/src/main/java/com/dxdevil/pd/prjp/ProfileActivity.kt
@@ -3,8 +3,6 @@ package com.dxdevil.pd.prjp
 import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import kotlinx.android.synthetic.main.activity_profile.*
 
 
 class ProfileActivity : AppCompatActivity() {
@@ -12,25 +10,14 @@ class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
-        setSupportActionBar(profileToolbar as Toolbar)
 
+        fun onCreateOptionsMenu(menu: Menu): Boolean {
+           val inflater = menuInflater
+           inflater.inflate(R.menu.edit_icon, menu)
+            return true
+       }
 
-
-        // Now get the support action bar
-        val actionBar = supportActionBar
-
-
-        actionBar!!.title = "Hello APP"
-
-
-        actionBar.elevation = 4.0F
-
-
-        actionBar.run {
-            setDisplayShowHomeEnabled(true)
-            setDisplayUseLogoEnabled(true)
-        }
-
+        onOptionsItemSelected()
 
 
     }
