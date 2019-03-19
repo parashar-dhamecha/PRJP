61fcc27ca57345a7ff0b493ee2c4748fe2a536d4
Pratik-Goswami <goswamipratik666@gmail.com>
2019-03-20 00:57:21 +0530

    profile activity recreated by pratik

--- app/src/main/java/com/dxdevil/pd/prjp/Settings.kt
+++ app/src/main/java/com/dxdevil/pd/prjp/Settings.kt
@@ -1,18 +1,15 @@
 package com.dxdevil.pd.prjp
 
-import android.os.Bundle
-
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_settings.*
 
-
-class ProfileActivity : AppCompatActivity() {
+class Settings : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        setContentView(R.layout.activity_settings)
 
 
     }
-
 }
-
