4702dad6bf17fc1366fbab85fd5f5c8f852e05f5
juhikaushik02 <juhikaushik02@gmail.com>
2019-03-19 10:52:05 +0530

    recyclerview contacts

---
+++ app/src/main/java/com/dxdevil/pd/prjp/AddContact.kt
@@ -0,0 +1,13 @@
+package com.dxdevil.pd.prjp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddContact : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_contact)
+
+    }
+}
---
+++ app/src/main/java/com/dxdevil/pd/prjp/AddGroup.kt
@@ -0,0 +1,12 @@
+package com.dxdevil.pd.prjp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddGroup : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_group)
+    }
+}
