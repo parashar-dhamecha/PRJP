1256604d25d01c648c8ecbe5c9ce01ae719870d4
Pratik-Goswami <goswamipratik666@gmail.com>
2019-03-20 01:18:38 +0530

    p

--- app/src/main/java/com/dxdevil/pd/prjp/Settings.kt
+++ app/src/main/java/com/dxdevil/pd/prjp/Settings.kt
@@ -1,8 +1,10 @@
 package com.dxdevil.pd.prjp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_settings.*
+
 
 class Settings : AppCompatActivity() {
 
@@ -10,6 +12,17 @@ class Settings : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
+        cardView.setOnClickListener {
+            startActivity(Intent(applicationContext,ProfileActivity::class.java))
+        }
+
+        cardView2.setOnClickListener {
+            startActivity(Intent(applicationContext,ChangePassword::class.java))
+        }
+
+        card_logout.setOnClickListener {
+            startActivity(Intent(applicationContext,LoginActivity::class.java))
+        }
 
     }
 }
