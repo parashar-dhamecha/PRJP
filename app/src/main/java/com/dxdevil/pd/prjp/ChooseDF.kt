6d86a2745e57f8c7d112b89babd4a95919eafa15
rishabh90993 <rishabh90993@gmail.com>
2019-03-19 17:12:48 +0530

    Rishabh                 popup added

--- app/src/main/java/com/dxdevil/pd/prjp/ChooseDF.kt
+++ app/src/main/java/com/dxdevil/pd/prjp/ChooseDF.kt
@@ -1,20 +1,42 @@
 package com.dxdevil.pd.prjp
 
-import android.app.Dialog
+
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
+import android.view.Gravity
+import android.R.attr.x
+import android.app.Dialog
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.signpopup.*
+
 
 class ChooseDF : DialogFragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        dialog!!.window!!.setGravity(Gravity.CENTER_HORIZONTAL)
 
-        val rootView = inflater.inflate(R.layout.signpopup, container)
-        return rootView
+        val p = dialog!!.window!!.attributes
+        p.width = ViewGroup.LayoutParams.MATCH_PARENT
+        p.softInputMode = WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE
+        dialog!!.window!!.setAttributes(p)
+        return inflater.inflate(R.layout.signpopup, container,false)
     }
+
+
 }
+
+
 
 
--- app/src/main/java/com/dxdevil/pd/prjp/Dashboard.kt
+++ app/src/main/java/com/dxdevil/pd/prjp/Dashboard.kt
@@ -1,19 +1,20 @@
 package com.dxdevil.pd.prjp
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.transition.Slide
-
+import android.util.Log
 import android.view.*
-
-
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
-
 import androidx.drawerlayout.widget.DrawerLayout
-
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_dashboard.*
-
+import kotlinx.android.synthetic.main.activity_profile.*
+import kotlinx.android.synthetic.main.signpopup.*
+import java.lang.Exception
 
 
 class Dashboard : AppCompatActivity() {
@@ -34,12 +35,19 @@ class Dashboard : AppCompatActivity() {
             Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
                 .setAction("Action", null)
                 .show()
-            var ft = supportFragmentManager
-            val cf:ChooseDF = ChooseDF()
-            addsign.setOnClickListener {
-                cf.show(ft,"choose fragment tag")
+        }
+
+        var ft = this.supportFragmentManager.beginTransaction()
+        val cf: ChooseDF = ChooseDF()
+
+        addsign.setOnClickListener {
+            try {
+                cf.show(ft, "dialog")
+            } catch (e: Exception) {
+                Log.d("1", "exception $e")
             }
         }
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
@@ -47,23 +55,6 @@ class Dashboard : AppCompatActivity() {
             return true
         return super.onOptionsItemSelected(item)
     }
-
-
-
-      /*  val draw = view.findViewById<Button>(R.id.draw_signature)
-        draw.setOnClickListener{
-            startActivity(Intent(getApplicationContext(),DrawSignature::class.java))
-        }
-        val pic = view.findViewById<Button>(R.id.photobutton)
-        pic.setOnClickListener {
-            this.startActivity(Intent(applicationContext,PhotoActivity::class.java))
-        }
-        val type = view.findViewById<Button>(R.id.typebutton)
-        type.setOnClickListener {
-            this.startActivity(Intent(applicationContext, Type::class.java))
-        }
-        */
-
 
     }
 
