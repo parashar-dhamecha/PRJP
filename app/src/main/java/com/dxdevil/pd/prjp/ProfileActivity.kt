fdec32b16c48b7ad9cb34d3a87cb59af125b4d50
Pratik-Goswami <goswamipratik666@gmail.com>
2019-03-25 23:24:52 +0530

    log in layout design is changed

--- app/src/main/java/com/dxdevil/pd/prjp/ProfileActivity.kt
+++ app/src/main/java/com/dxdevil/pd/prjp/ProfileActivity.kt
@@ -1,6 +1,7 @@
 package com.dxdevil.pd.prjp
 
 import android.os.Bundle
+import android.view.Menu
 
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,9 +11,13 @@ class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
-
-
     }
 
+   override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu to use in the action bar
+        val inflater = menuInflater
+        inflater.inflate(R.menu.edit_icon, menu)
+        return true
+    }
 }
 
