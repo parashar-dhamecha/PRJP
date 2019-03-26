9d1876df71dbcb33d14c91ededb6fa206f88c553
Pratik-Goswami <goswamipratik666@gmail.com>
2019-03-26 23:22:02 +0530

    Register layout changed

--- app/src/main/java/com/dxdevil/pd/prjp/LoginActivity.kt
+++ app/src/main/java/com/dxdevil/pd/prjp/LoginActivity.kt
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import com.google.android.material.textfield.TextInputEditText
 import kotlinx.android.synthetic.main.activity_login.*
 
 @SuppressLint("Registered")
@@ -12,6 +14,13 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        var UsernameEmail = findViewById<EditText>(R.id.UsernameEmail)
+        var edPassword = findViewById<EditText>(R.id.edPassword)
+
+        var uname = UsernameEmail.text.toString()
+        var pass:String = edPassword.text.toString()
+
 
         Login_Button.setOnClickListener {
             startActivity(Intent(applicationContext, Dashboard::class.java))
