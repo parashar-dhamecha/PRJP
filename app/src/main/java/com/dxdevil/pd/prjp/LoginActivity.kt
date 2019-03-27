09ea60abd3af695f018795ac1ba848bf6f372406
Pratik-Goswami <goswamipratik666@gmail.com>
2019-03-27 22:14:46 +0530

    Register

--- app/src/main/java/com/dxdevil/pd/prjp/LoginActivity.kt
+++ app/src/main/java/com/dxdevil/pd/prjp/LoginActivity.kt
@@ -5,8 +5,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.Toast
+import com.dxdevil.pd.prjp.Model.Request.Login
+import com.dxdevil.pd.prjp.Model.Response.LoginModel
 import com.google.android.material.textfield.TextInputEditText
 import kotlinx.android.synthetic.main.activity_login.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 @SuppressLint("Registered")
 class LoginActivity : AppCompatActivity() {
@@ -19,25 +25,31 @@ class LoginActivity : AppCompatActivity() {
         var edPassword = findViewById<EditText>(R.id.edPassword)
 
         var uname = UsernameEmail.text.toString()
-        var pass:String = edPassword.text.toString()
+        var pass: String = edPassword.text.toString()
 
 
         Login_Button.setOnClickListener {
             startActivity(Intent(applicationContext, Dashboard::class.java))
-        Login_Button?.setOnClickListener {
-           var call :Call<LoginModel> = RetrofitClient.getInstance().api.login(Login(UsernameEmail.text.toString(),edPassword.text.toString()))
-            call.enqueue(object : Callback<LoginModel> {
-                override fun onFailure(call: Call<LoginModel>, t: Throwable) {
-                    Toast.makeText(this@LoginActivity,"faliure",Toast.LENGTH_LONG).show()
-                }
+            Login_Button?.setOnClickListener {
+                var call: Call<LoginModel> = RetrofitClient.getInstance().api.login(
+                    Login(
+                        UsernameEmail.text.toString(),
+                        edPassword.text.toString()
+                    )
+                )
+                call.enqueue(object : Callback<LoginModel> {
+                    override fun onFailure(call: Call<LoginModel>, t: Throwable) {
+                        Toast.makeText(this@LoginActivity, "faliure", Toast.LENGTH_LONG).show()
+                    }
 
-                override fun onResponse(call: Call<LoginModel>, response: Response<LoginModel>) {
+                    override fun onResponse(call: Call<LoginModel>, response: Response<LoginModel>) {
 
-                    Toast.makeText(this@LoginActivity,"success",Toast.LENGTH_LONG).show()
-                }
+                        Toast.makeText(this@LoginActivity, "success", Toast.LENGTH_LONG).show()
+                    }
 
-            })
+                })
+            }
+
         }
-
     }
 }
--- app/src/main/java/com/dxdevil/pd/prjp/Registration.kt
+++ app/src/main/java/com/dxdevil/pd/prjp/Registration.kt
@@ -1,15 +1,115 @@
 package com.dxdevil.pd.prjp
-
-import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_registration.*
 
-@SuppressLint("Registered")
+
+
+val Passwordregex = """^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@${'$'}!%*?&])[A-Za-z\d@${'$'}!%*?&]{8,}${'$'}""".toRegex()
+val Nameregex="""^[A-Za-z]+${'$'}""".toRegex()
+val Numberregex="""^[0-9]+$""".toRegex()
+
+
+
 class Registration : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
 
+//        val etFirstName= findViewById<TextInputEditText>(R.id.etFirstName)
+//        val etLastName= findViewById<TextInputEditText>(R.id.etLastName)
+//        val etEmail= findViewById<TextInputEditText>(R.id.etEmail)
+//        val etMobileNo= findViewById<TextInputEditText>(R.id.etMobileNo)
+//        val etPassword= findViewById<TextInputEditText>(R.id.etPassword)
+//        val etConfirmPassword= findViewById<TextInputEditText>(R.id.etConfirmPassword)
+//        val btregister=findViewById<Button>(R.id.btregister)
+//        var loginlink=findViewById<TextView>(R.id.loginlink)
+
+//        var Fname = etFirstName!!.text.toString()
+//        var Lname = etLastName!!.text.toString()
+//        var email = etEmail!!.text.toString()
+//        var MobileNo = etMobileNo!!.text.toString()
+//        var Password = etPassword!!.text.toString()
+//        var ConfirmPassword = etConfirmPassword!!.text.toString()
+
+
+
+
+        btregister!!.setOnClickListener {
+
+            if(validation())
+            {
+                startActivity(Intent(applicationContext,LoginActivity::class.java))
+            }
+        }
+
+
     }
+
+    private fun validation():Boolean{
+
+            var valid=false
+
+
+            val fname= etFirstName!!.text.toString()
+            val lname= etLastName!!.text.toString()
+            val email= etEmail!!.text.toString()
+            val mobileNo= etMobileNo!!.text.toString()
+            val password= etPassword!!.text.toString()
+            val confirmPassword= etConfirmPassword!!.text.toString()
+
+        var flagFirst =false
+        var flagLast =false
+        var flagEmail =false
+        var flagMobile =false
+        var flagPassword =false
+        var flagConfirm =false
+
+
+            if(fname.isEmpty()|| !Nameregex.matches(fname))
+            {
+                etFirstName!!.error="Enter a valid First Name"
+            }
+            else {flagFirst =true}
+
+
+            if(lname.isEmpty()||!Nameregex.matches(lname))
+            {
+                etLastName!!.error="Enter a valid Last name"
+            }
+             else{ flagLast=true }
+
+            if(mobileNo.isEmpty()||mobileNo.length>15||!Numberregex.matches(mobileNo))
+            {
+                etMobileNo!!.error="Enter a valid MobileNo"
+            }
+            else{flagMobile=true }
+
+          if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+               etEmail!!.error="Enter a valid Email Address"
+               valid= false
+          }
+            else{flagEmail=true}
+
+          if(password.isEmpty() || password.length<8 || !Passwordregex.matches(password)) {
+
+               etPassword!!.error = "Enter a valid Password"
+            }
+
+            else
+            { flagPassword=true}
+
+            if(confirmPassword.isEmpty() || confirmPassword.length<8|| confirmPassword!=password) {
+                etConfirmPassword!!.error = "Password Do not match"
+            }
+            else
+            { flagConfirm=true }
+
+        if(flagFirst&&flagLast&&flagEmail&&flagPassword&&flagConfirm&&flagMobile){valid =true}
+        return valid
+
+   }
+
 }
