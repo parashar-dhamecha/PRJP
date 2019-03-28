06366bff096649a847aa724af188016c3cec2de7
rishabh90993 <rishabh90993@gmail.com>
2019-03-28 12:33:43 +0530

    Rishabh login api integerated updated

--- app/src/main/java/com/dxdevil/pd/prjp/Api.java
+++ app/src/main/java/com/dxdevil/pd/prjp/Api.java
@@ -5,9 +5,12 @@ import com.dxdevil.pd.prjp.Model.Response.LoginModel;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
 public interface Api {
-    @POST("Acount/login")
-    Call<LoginModel>login(@Body Login alogin);
+
+    @POST("api/account/login")
+    Call<LoginModel>login(@Body Login login );
 }
--- app/src/main/java/com/dxdevil/pd/prjp/LoginActivity.kt
+++ app/src/main/java/com/dxdevil/pd/prjp/LoginActivity.kt
@@ -1,43 +1,115 @@
 package com.dxdevil.pd.prjp
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.Preference
+import android.util.Log
+import android.util.Patterns
 import android.widget.EditText
-import com.google.android.material.textfield.TextInputEditText
+import android.widget.Toast
+import com.dxdevil.pd.prjp.Model.Request.Login
+import com.dxdevil.pd.prjp.Model.Response.LoginModel
+
 import kotlinx.android.synthetic.main.activity_login.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.lang.Exception
+
 
 @SuppressLint("Registered")
 class LoginActivity : AppCompatActivity() {
 
+    var passpattern: Regex =
+        """^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@${'$'}!%*?&])[A-Za-z\d@${'$'}!%*?&]{8,}${'$'}""".toRegex()
+    var pref: SharedPreferences? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        pref = applicationContext!!.getSharedPreferences("Tokenpref", 0)
+        var edpref: SharedPreferences.Editor = pref!!.edit()
 
-        var UsernameEmail = findViewById<EditText>(R.id.UsernameEmail)
+        var UsernameEmail = findViewById<EditText>(R.id.edEmail)
         var edPassword = findViewById<EditText>(R.id.edPassword)
 
         var uname = UsernameEmail.text.toString()
-        var pass:String = edPassword.text.toString()
+        var pass: String = edPassword.text.toString()
 
+        Login_Button!!.setOnClickListener {
+            var email = edEmail!!.text?.toString()
+            var password = edPassword!!.text?.toString()
+            if (validateemail() && validatepass()) {
 
-        Login_Button.setOnClickListener {
-            startActivity(Intent(applicationContext, Dashboard::class.java))
-        Login_Button?.setOnClickListener {
-           var call :Call<LoginModel> = RetrofitClient.getInstance().api.login(Login(UsernameEmail.text.toString(),edPassword.text.toString()))
-            call.enqueue(object : Callback<LoginModel> {
-                override fun onFailure(call: Call<LoginModel>, t: Throwable) {
-                    Toast.makeText(this@LoginActivity,"faliure",Toast.LENGTH_LONG).show()
-                }
+               var api1 = RetrofitClient.getInstance().api as Api
+                var call  = api1.login(Login(email, password)) as Call<LoginModel>
+                call!!.enqueue(object : Callback<LoginModel> {
+                    override fun onFailure(call: Call<LoginModel>, t: Throwable) {
+                        call.cancel()
+                        Toast.makeText(this@LoginActivity, "faliure", Toast.LENGTH_LONG).show()
+                    }
 
-                override fun onResponse(call: Call<LoginModel>, response: Response<LoginModel>) {
+                    override fun onResponse(call: Call<LoginModel>, response: Response<LoginModel>) {
+                        if (response.isSuccessful) {
+                            try {
+                                var lm=response.body()
+                                Log.d("30", "error")
+                                var t = lm!!.data[0]?.token?.toString()
+                                Log.d("1","error")
+                               var rt = lm!!.data[0]?.refreshToken?.toString()
+                                Log.d("2","error")
+                                edpref.putString("Token", t)
+                                Log.d("3","error")
+                                edpref.putString("RefreshToken", rt)
+                                Log.d("4","error")
 
-                    Toast.makeText(this@LoginActivity,"success",Toast.LENGTH_LONG).show()
-                }
+                                startActivity(Intent(this@LoginActivity, Dashboard::class.java))
+                                Toast.makeText(this@LoginActivity, "succcess", Toast.LENGTH_LONG).show()
+                            } catch (e: Exception) {
+                                Toast.makeText(this@LoginActivity, "exception thrown :$e", Toast.LENGTH_LONG).show()
+                            }
+                        } else {
+                            Toast.makeText(this@LoginActivity, "error", Toast.LENGTH_LONG).show()
+                        }
 
-            })
+                    }
+                })
+            }
+
+        }
+    }
+
+    fun validateemail(): Boolean {
+        var email = edEmail.text.toString()
+        if (email == "") {
+            edEmail.setError("Email address cant be empty")
+            return false
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            edEmail.setError("Enter a valid email address")
+            return false
+        } else {
+            return true
         }
 
     }
-}
+
+        fun validatepass(): Boolean {
+            val pass = edPassword.text.toString()
+            if (pass == "") {
+                edPassword.setError("Password cant be empty")
+                return false
+            } else if (!passpattern.matches(pass)) {
+                edPassword.setError("Enter a valid Password")
+                return false
+            } else {
+                return true
+            }
+
+        }
+    }
