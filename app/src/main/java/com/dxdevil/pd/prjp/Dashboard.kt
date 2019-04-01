671c4998f8bda347c75128f3ccfb1949ca58cf73
Pratik-Goswami <goswamipratik666@gmail.com>
2019-04-01 18:02:56 +0530

    Merge branch 'master' of E:\projects\PRJP with conflicts.

--- app/src/main/java/com/dxdevil/pd/prjp/Dashboard.kt
+++ app/src/main/java/com/dxdevil/pd/prjp/Dashboard.kt
@@ -1,75 +1,109 @@
 package com.dxdevil.pd.prjp
 
 
+import android.content.Context
 import android.content.Intent
+
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.transition.Slide
+
 import android.view.*
-import android.widget.Toast
+import android.widget.*
+
+
 import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.cardview.widget.CardView
-import androidx.core.view.GravityCompat
+import androidx.core.view.marginLeft
+
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_dashboard.*
+import kotlinx.android.synthetic.main.activity_profile.*
 import kotlinx.android.synthetic.main.signpopup.*
-import java.lang.Exception
+import kotlinx.android.synthetic.main.signpopup.view.*
 
 
 class Dashboard : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
-    private lateinit var ntoggle: ActionBarDrawerToggle
+    private lateinit var ntoggle : ActionBarDrawerToggle
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
         drawerLayout = findViewById(R.id.drawerlayoutid)
-        ntoggle= ActionBarDrawerToggle(this,drawerLayout,R.string.navigation_drawer_open,R.string.navigation_drawer_close)
+        ntoggle= ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
         drawerLayout.addDrawerListener(ntoggle)
         ntoggle.syncState()
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-        uploadcv.setOnClickListener { view ->
-           startActivity(Intent(applicationContext,SetAnnotation::class.java))
+//         uploadcv.setOnClickListener { view ->
+//            Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null)
+//                .show()
+//            }
         }
 
-        draw_signature?.setOnClickListener {
-            Toast.makeText(applicationContext, "hello", Toast.LENGTH_LONG)
-            startActivity(Intent(applicationContext, DrawSignature::class.java))
-        }
-        photobutton?.setOnClickListener {
-            startActivity(Intent(applicationContext, PhotoActivity::class.java))
-        }
-        typebutton?.setOnClickListener {
-            startActivity(Intent(applicationContext, Type::class.java))
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if(ntoggle.onOptionsItemSelected(item))
+            return true
+        return super.onOptionsItemSelected(item)
+    }
+    fun showdiag(view:View) {
+        val inflater:LayoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+        // Inflate a custom view using layout inflater
+        var view = inflater.inflate(R.layout.signpopup,null)
+        // Initialize a new instance of popup window
+        val popupWindow = PopupWindow(
+            view, // Custom view to show in popup window
+            LinearLayout.LayoutParams.MATCH_PARENT, // Width of popup window
+            LinearLayout.LayoutParams.WRAP_CONTENT // Window height
+        )
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            popupWindow.elevation = 10.0F
         }
 
-        addsign.setOnClickListener {
-            try {
-                var ft = supportFragmentManager.beginTransaction()
-                val cf: ChooseDF = ChooseDF()
-                cf.show(ft, "dialog")
 
-            } catch (e: Exception) {
-                Log.d("1", "exception $e")
-            }
+        // If API level 23 or higher then execute the code
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+
+            // Create a new slide animation for popup window enter transition
+            val slideIn = Slide()
+            slideIn.slideEdge = Gravity.TOP
+            popupWindow.enterTransition = slideIn
+
+            // Slide animation for popup window exit transition
+            val slideOut = Slide()
+            slideOut.slideEdge = Gravity.RIGHT
+            popupWindow.exitTransition = slideOut
+
         }
+        val tv = view.findViewById<TextView>(R.id.close_tvid)
+        tv.setOnClickListener {
+            popupWindow.dismiss()
+
+        }
+        val draw = view.findViewById<Button>(R.id.draw_signature)
+        draw.setOnClickListener{
+            startActivity(Intent(getApplicationContext(),DrawSignature::class.java))
+        }
+        val pic = view.findViewById<Button>(R.id.photobutton)
+        pic.setOnClickListener {
+            this.startActivity(Intent(applicationContext,PhotoActivity::class.java))
+        }
+        val type = view.findViewById<Button>(R.id.typebutton)
+        type.setOnClickListener {
+            this.startActivity(Intent(applicationContext, Type::class.java))
+        }
+        var vg:ViewGroup =findViewById(R.id.linearLayout)
+
+        popupWindow.showAtLocation(vg,Gravity.CENTER,0,0)
+        popupWindow.isFocusable
 
     }
-
-
-
-        override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-            if(ntoggle.onOptionsItemSelected(item))
-                return true
-            return super.onOptionsItemSelected(item)
-        }
 }
-
-
-
-
-
 
 
 
