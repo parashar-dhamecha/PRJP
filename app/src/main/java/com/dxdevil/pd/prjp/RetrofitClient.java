e9cab664b6915e89336d386d50856ae462e73d97
juhikaushik02 <juhikaushik02@gmail.com>
2019-04-03 11:32:07 +0530

    contacts

---
+++ app/src/main/java/com/dxdevil/pd/prjp/RetrofitClient.java
@@ -0,0 +1,24 @@
+package com.dxdevil.pd.prjp;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static String BASE_URL ="https://cygnatureapipoc.stagingapplications.com/";
+    private static RetrofitClient minstance;
+    private static Retrofit retrofit;
+    private RetrofitClient(){
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create()).build();
+    }
+    public static synchronized RetrofitClient getInstance(){
+        if(minstance == null)  {
+            minstance=new RetrofitClient();
+        }
+        return minstance;
+    }
+    public Api getApi(){
+        return retrofit.create(Api.class);
+    }
+}
