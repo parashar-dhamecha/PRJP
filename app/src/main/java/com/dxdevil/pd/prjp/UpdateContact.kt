9aace3d750571373d472cc4f1d6600f24e6cf434
juhikaushik02 <juhikaushik02@gmail.com>
2019-04-21 09:37:20 +0530

    contacts

---
+++ app/src/main/java/com/dxdevil/pd/prjp/UpdateContact.kt
@@ -0,0 +1,12 @@
+package com.dxdevil.pd.prjp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UpdateContact : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_contact)
+    }
+}
