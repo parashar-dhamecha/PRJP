c70dbb7b57c379ad8ef73f0efd64f8420d55b164
rishabh90993 <rishabh90993@gmail.com>
2019-05-03 20:45:54 +0530

    Rishabh new view created

--- app/src/main/java/com/dxdevil/pd/prjp/Annotation2.kt
+++ app/src/main/java/com/dxdevil/pd/prjp/Annotation2.kt
@@ -9,17 +9,18 @@ import android.widget.TextView
 import android.widget.RelativeLayout
 import android.view.MotionEvent
 import android.view.View
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_annotation2.*
 
 
 @Suppress("ImplicitThis")
 class Annotation2 : AppCompatActivity(),View.OnTouchListener {
     lateinit var view: ImageView
-    lateinit var view1: ImageView
     lateinit var root: ViewGroup
-    lateinit var root2: ViewGroup
     private var _xDelta: Int = 0
     private var _yDelta: Int = 0
+     var arr : ArrayList<String> = ArrayList()
+
     @SuppressLint("ClickableViewAccessibility", "ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -27,10 +28,15 @@ class Annotation2 : AppCompatActivity(),View.OnTouchListener {
         setContentView(R.layout.activity_annotation2)
         root = findViewById(R.id.Relativelid)
 
+        arr.add("Rishabh")
+        arr.add("Ashish")
+        arr.add("kalpesh")
+
+        var adapter:ArrayAdapter<String> = ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,arr)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        signerspinner.adapter=adapter
         view = ImageView(this)
         view.setImageDrawable(getDrawable(R.drawable.logo))
-        view1 = ImageView(this)
-        view1.setImageDrawable(getDrawable(R.drawable.logo))
 
         val layoutParams = RelativeLayout.LayoutParams(500, 200)
         layoutParams.leftMargin = 0
@@ -42,12 +48,45 @@ class Annotation2 : AppCompatActivity(),View.OnTouchListener {
         view.x= 100F
         view.y=100F
         previewdocid.setImageDrawable(getDrawable(R.drawable.sampledoc))
-        view.setOnClickListener {
-    view.setOnTouchListener(this)
-       }
 
-        root.addView(view)
+        addantbutton.setOnClickListener {
+            view = addannotatio() as ImageView
+            root.addView(view)
+            view.setOnClickListener {
+                view.setOnTouchListener(this)
+            }
+        }
+        clearantbutton.setOnClickListener {
+            root.removeView(view)
+        }
+        clearallantbutton.setOnClickListener {
+            root.removeAllViews()
+        }
+
+
+
     }
+
+    private fun addannotatio():View {
+        view = ImageView(this)
+        view.setImageDrawable(getDrawable(R.drawable.logo))
+        setLayoutsize(400,170)
+        return view
+    }
+
+    @SuppressLint("ResourceAsColor")
+    private fun setLayoutsize(width:Int,height:Int){
+        val layoutParams = RelativeLayout.LayoutParams(width, height)
+        layoutParams.leftMargin = 0
+        layoutParams.topMargin = 0
+        layoutParams.bottomMargin = -250
+        layoutParams.rightMargin = -250
+        view.setLayoutParams(layoutParams)
+        view.setBackgroundColor(R.color.DueSoon)
+        view.x= 100F
+        view.y=100F
+    }
+
     override fun onTouch(view: View, event: MotionEvent): Boolean {
         val X = event.rawX.toInt()
         val Y = event.rawY.toInt()
@@ -58,10 +97,13 @@ class Annotation2 : AppCompatActivity(),View.OnTouchListener {
                 _yDelta = Y - lParams.topMargin
             }
             MotionEvent.ACTION_UP -> {
+
             }
             MotionEvent.ACTION_POINTER_DOWN -> {
+
             }
             MotionEvent.ACTION_POINTER_UP -> {
+
             }
             MotionEvent.ACTION_MOVE -> {
                 val layoutParams = view.getLayoutParams() as RelativeLayout.LayoutParams
